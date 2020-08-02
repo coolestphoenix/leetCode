a984ce19a1867915bcae18a456abda88efc2e484
Umair Manzoor <coolest.phoenix@outlook.com>
2020-08-02 11:03:15 +0530

    704. Binary Search leetCode

---
+++ BinarySearch.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int start =0;
+        int end = nums.Length -1;
+        while (start <= end){
+            int middle = (start + end) / 2;
+            if(nums[middle] == target){
+                return middle;
+            }
+            else if(nums[middle] > target){
+                end = middle - 1;
+            }
+            else {
+                start = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
